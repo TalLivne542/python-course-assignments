5fbf5df898215dc08a601ad78d93ceb0e370efe8
TalLivne542 <Tal.livne@weizmann.ac.il>
2026-04-20 16:33:08 +0300

    Create crystallinity_cli_calc.py

---
+++ day02/crystallinity_cli_calc.py
@@ -0,0 +1,18 @@
+import sys
+
+POLYMER_DATA = {"PE": 293.0, "PP": 207.0, "PET": 140.0, "Nylon6": 230.0}
+
+if len(sys.argv) != 3:
+    print("Usage: python cli_calc.py [Polymer_Name] [Enthalpy_Value]")
+    print("Example: python cli_calc.py PE 150")
+else:
+    poly = sys.argv[1]
+    try:
+        hm = float(sys.argv[2])
+        if poly in POLYMER_DATA:
+            xc = (hm / POLYMER_DATA[poly]) * 100
+            print(f"CLI Result: {poly} Crystallinity = {xc:.2f}%")
+        else:
+            print(f"Error: {poly} is not in the database.")
+    except ValueError:
+        print("Error: Enthalpy must be a number.")
